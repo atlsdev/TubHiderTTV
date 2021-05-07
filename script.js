aa3d651074fc55ce52d9884de1b1b7038514fa15
Atlas <68791999+Atlas7005@users.noreply.github.com>
2021-05-07 23:50:54 +0200

    changing directory will now work

--- script.js
+++ script.js
@@ -1,34 +1,46 @@
 const USERNAME_SEL = "[data-test-selector=\"ChannelLink\"]";
-// const BL_URL = "https://raw.githubusercontent.com/atlsdev/ThotHiderTTV/master/blacklist.json";
-const BL_URL = "https://cdn.jsdelivr.net/gh/atlsdev/ThotHiderTTV/blacklist.json";
+const BL_URL = "https://raw.githubusercontent.com/atlsdev/ThotHiderTTV/master/blacklist.json";
+// const BL_URL = "https://cdn.jsdelivr.net/gh/atlsdev/ThotHiderTTV/blacklist.json";
 
 window.onload = load();
+var curHref = location.href;
 
 const log = (...msg) => console.log("[THOT HIDER]",...msg);
 
 function load() {
+    new MutationObserver(mutations => {
+        mutations.forEach(() => {
+            if(curHref !== location.href) {
+                curHref = location.href;
+                load();
+            }
+        });
+    }).observe(document.body, {
+        childList: true,
+        subtree: true
+    });
     if(window.blacklist === undefined || window.blacklist === null) {
         var xhr = new XMLHttpRequest();
         xhr.onreadystatechange = function() {
             if (this.readyState == 4 && this.status == 200) {
                 window.blacklist = JSON.parse(this.responseText);
-                waitFor(`div[data-target="directory-first-item"]`).then(() => {
-                    var channels = document.querySelectorAll(USERNAME_SEL);
-                    channels.forEach((channel) => hide(channel));
-                    new MutationObserver((mutations) => {
-                        mutations.forEach((mutation) => {
-                            if(mutation.addedNodes.length > 0) {
-                                channels = document.querySelectorAll(USERNAME_SEL);
-                                channels.forEach((channel) => hide(channel));
-                            }
-                        });
-                    }).observe(document.querySelector(".hRbnOC"), { childList: true });
-                });
             }
         };
         xhr.open("GET", BL_URL, true);
         xhr.send();
     }
+    waitFor(`div[data-target="directory-first-item"]`).then(() => {
+        var channels = document.querySelectorAll(USERNAME_SEL);
+        channels.forEach((channel) => hide(channel));
+        new MutationObserver((mutations) => {
+            mutations.forEach((mutation) => {
+                if(mutation.addedNodes.length > 0) {
+                    channels = document.querySelectorAll(USERNAME_SEL);
+                    channels.forEach((channel) => hide(channel));
+                }
+            });
+        }).observe(document.querySelector(".hRbnOC"), { childList: true });
+    });
 };
 
 function hide(node) {
